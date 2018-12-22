290cb9d1b1100de790ec53b45d09a2991b7f14bc
chenjunfei <chenjunfei@bkrwin.com>
2018-12-22 19:56:36 +0800

    添加日志

--- src/com/panda521/SpringDemoApplication.java
+++ src/com/panda521/SpringDemoApplication.java
@@ -1,19 +1,23 @@
 package com.panda521;
 
 import com.panda521.vo.UserVO;
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * @author chenjunfei
  * @date 2018-12-22
  */
 public class SpringDemoApplication {
+
+    public static Logger logger = Logger.getLogger(SpringDemoApplication.class);
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserVO userVO =(UserVO)applicationContext.getBean("user");
         System.out.println(userVO);
-        
-
+        logger.info("日志打印了"+userVO);
     }
 }
